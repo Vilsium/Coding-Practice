3883ca2a9b54bc61aed6984f88118fa62ac216aa
Vilsium <vilsium28@gmail.com>
2025-05-28 13:31:23 +0530

    Printign a pattern

---
+++ 28th May, 2025/printPyramid.c
@@ -0,0 +1,21 @@
+/*
+Pattern to be printed
+    *
+   ***
+  *****
+ *******
+*/
+
+#include <stdio.h>
+
+int main() {
+    for(int i = 1; i <= 4; i++) {
+        for(int j = 0; j < 4-i; j++) {
+            printf(" ");
+        }
+        for(int j = 0; j < 2*i-1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
