aa39adcd29ff3e1a52d8b69d3bfcc3d5d757c8c4
Vilsium <vilsium28@gmail.com>
2025-06-10 23:11:06 +0530

    Assignment completed

--- NodeJS Course/Assignment 2/app.js
+++ NodeJS Course/Assignment 2/app.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("Middleware no. 1");
-  next();
+// app.use((req, res, next) => {
+//   console.log("Middleware no. 1");
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log("Middleware no. 2");
+//   res.send("<h1>Assignment 2 - task 2</h1>");
+// });
+
+app.use("/users", (req, res, next) => {
+  res.send("<h1>Welcome to the users page of Assignment 2</h1>");
 });
 
-app.use((req, res, next) => {
-  console.log("Middleware no. 2");
-  res.send("<h1>Assignment 2 - task 2</h1>");
+app.use("/", (req, res, next) => {
+  res.send("<h1>Welcome to the root page of Assignment 2</h1>");
 });
 
 app.listen(3000);
