33c3c08f514467fa3db618fee08f0115980d5db1
Vilsium <vilsium28@gmail.com>
2025-06-15 21:26:56 +0530

    Enhanced the app

---
+++ NodeJS Course/Practice Project/controllers/shop.js
@@ -0,0 +1,33 @@
+const Product = require("../models/product");
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAllProducts((products) => {
+    res.render("shop/product-list", {
+      products: products,
+      pageTitle: "All Products",
+      path: "/products",
+    });
+  });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAllProducts((products) => {
+    res.render("shop/index", {
+      products: products,
+      pageTitle: "Home",
+      path: "/",
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render("shop/cart", { pageTitle: "Your Cart", path: "/cart" });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", { pageTitle: "Orders", path: "/orders" });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render("shop/checkout", { pageTitle: "Checkout", path: "/checkout" });
+};
